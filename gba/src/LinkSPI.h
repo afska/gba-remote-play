78e2d3b6b9f4b66eabc2633c7cdcd51f81e28501
Rodrigo Alfonso <rodri042@gmail.com>
2021-05-21 22:14:49 -0300

    Refactor: Moving code to LinkSPI.h

---
+++ gba/src/LinkSPI.h
@@ -0,0 +1,71 @@
+#ifndef LINK_SPI_H
+#define LINK_SPI_H
+
+#include <tonc.h>
+
+#define SPI_BIT_CLOCK 0
+#define SPI_BIT_SPEED 1
+#define SPI_BIT_SI 2
+#define SPI_BIT_SO 3
+#define SPI_BIT_START 7
+#define SPI_BIT_LENGTH 12
+#define SPI_SET_HIGH(REG, BIT) REG |= 1 << BIT
+#define SPI_SET_LOW(REG, BIT) REG &= ~(1 << BIT)
+#define SPI_IS_HIGH(REG, BIT) ((REG >> BIT) & 1)
+
+// A Link Port connection for Normal mode (slave, 2Mhz speed, 32bit packets)
+
+class LinkSPI {
+ public:
+  u32 transfer(u32 value) {
+    // while (!isReady())
+    //   ;
+
+    setNormalMode();
+    set2MhzSpeed();
+    set32BitPackets();
+    setData(value);
+    disableTransfer();
+
+    setSlaveMode();
+    enableTransfer();
+    startTransfer();
+
+    while (!isReady())
+      ;
+
+    return getData();
+  }
+
+  // void _onSerial() {
+  //   // TODO: IMPLEMENT
+  // }
+
+ private:
+  bool isWaiting = false;
+
+  void setNormalMode() {
+    REG_RCNT = 0;
+    REG_SIOCNT = 0;
+  }
+
+  void setData(u32 data) { REG_SIODATA32 = data; }
+  u32 getData() { return REG_SIODATA32; }
+
+  void enableTransfer() { SPI_SET_LOW(REG_SIOCNT, SPI_BIT_SO); }
+  void disableTransfer() { SPI_SET_HIGH(REG_SIOCNT, SPI_BIT_SO); }
+  void startTransfer() { SPI_SET_HIGH(REG_SIOCNT, SPI_BIT_START); }
+  bool isReady() { return !SPI_IS_HIGH(REG_SIOCNT, SPI_BIT_START); }
+
+  void set2MhzSpeed() { SPI_SET_HIGH(REG_SIOCNT, SPI_BIT_SPEED); }
+  void set32BitPackets() { SPI_SET_HIGH(REG_SIOCNT, SPI_BIT_LENGTH); }
+  void setSlaveMode() { SPI_SET_LOW(REG_SIOCNT, SPI_BIT_CLOCK); }
+};
+
+extern LinkSPI* linkSPI;
+
+// inline void SPI_ISR_SERIAL() {
+//   linkSPI->_onSerial();
+// }
+
+#endif  // LINK_SPI_H
--- gba/src/main.cpp
+++ gba/src/main.cpp
@@ -1,77 +1,20 @@
 #include <tonc.h>
 
 #include <string>
+#include "LinkSPI.h"
 
 void init();
 void log(std::string text);
 
-#define IS_MASTER true
-#define BIT_CLOCK 0
-#define BIT_SPEED 1
-#define BIT_SI 2
-#define BIT_SO 3
-#define BIT_START 7
-#define BIT_LENGTH 12
+LinkSPI* linkSPI = new LinkSPI();
 
 int main() {
   init();
 
-  log("Hello world");
-
-  u32 step = 0;
+  log("Waiting...");
+  log(std::to_string(linkSPI->transfer(0x12345678)));
 
   while (true) {
-  next:
-    if (step == 0) {
-      REG_RCNT = 0;
-      log("Set RCNT=0 (NORMAL)");
-      step++;
-      goto next;
-    } else if (step == 1) {
-      REG_SIOCNT = 0;
-      REG_SIOCNT |= (1 << BIT_SPEED);
-      log("Set SIOCNT<1>=1 (2Mhz)");
-      step++;
-      goto next;
-    } else if (step == 2) {
-      REG_SIOCNT |= 1 << BIT_LENGTH;
-      log("Set SIOCNT<12>=1 (32bit)");
-      step++;
-      goto next;
-    } else if (step == 3) {
-      REG_SIODATA32 = 0x12345678;
-      log("Set SIODATA32=...");
-      step++;
-      goto next;
-    } else if (step == 4) {
-      REG_SIOCNT |= 1 << BIT_SO;
-      log("Set SIOCNT<3>=1 (SO=HI)");
-      step++;
-      goto next;
-    } else if (step == 5) {
-      REG_SIOCNT &= ~(1 << BIT_CLOCK);
-      log("Set SIOCNT<0>=0 (EXT-slve)");
-      step++;
-      goto next;
-    } else if (step == 6) {
-      REG_SIOCNT &= ~(1 << BIT_SO);
-      log("Set SIOCNT<3>=0 (SO=LOW");
-      step++;
-      goto next;
-    } else if (step == 7) {
-      REG_SIOCNT |= 1 << BIT_START;
-      log("Set SIOCNT<7>=1 (START=HI)");
-      log("Waiting...");
-      step++;
-      goto next;
-    } else if (step == 8) {
-      if ((REG_SIOCNT & (1 << BIT_START)) == 0) {
-        log("(!) START low");
-        log("Data: " + std::to_string(REG_SIODATA32));
-        step++;
-      }
-    }
-
     VBlankIntrWait();
   }
 
