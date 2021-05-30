fa04b954e5ea8ab7f62f20dba0c9e55b740e22b3
Rodrigo Alfonso <rodri042@gmail.com>
2021-05-30 02:56:54 -0300

    Stop setting 2Mhz speed on GBA side: slaves must use the external clock

--- gba/src/LinkSPI.h
+++ gba/src/LinkSPI.h
@@ -4,7 +4,6 @@
 #include <tonc.h>
 
 #define SPI_BIT_CLOCK 0
-#define SPI_BIT_SPEED 1
 #define SPI_BIT_SI 2
 #define SPI_BIT_SO 3
 #define SPI_BIT_START 7
@@ -13,13 +12,12 @@
 #define SPI_SET_LOW(REG, BIT) REG &= ~(1 << BIT)
 #define SPI_IS_HIGH(REG, BIT) ((REG >> BIT) & 1)
 
-// A Link Port connection for Normal mode (slave, 2Mhz speed, 32bit packets)
+// A Link Port connection for Normal mode (slave, 32bit packets)
 
 class LinkSPI {
  public:
   u32 transfer(u32 value) {
     setNormalMode();
-    set2MhzSpeed();
     set32BitPackets();
     setSlaveMode();
     disableTransfer();
@@ -53,7 +51,6 @@ class LinkSPI {
   void startTransfer() { SPI_SET_HIGH(REG_SIOCNT, SPI_BIT_START); }
   bool isReady() { return !SPI_IS_HIGH(REG_SIOCNT, SPI_BIT_START); }
 
-  void set2MhzSpeed() { SPI_SET_HIGH(REG_SIOCNT, SPI_BIT_SPEED); }
   void set32BitPackets() { SPI_SET_HIGH(REG_SIOCNT, SPI_BIT_LENGTH); }
   void setSlaveMode() { SPI_SET_LOW(REG_SIOCNT, SPI_BIT_CLOCK); }
 };
