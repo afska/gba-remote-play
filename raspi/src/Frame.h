0a578d9f05b50e2946a2bd96d613b381edd3c77c
Rodrigo Alfonso <rodri042@gmail.com>
2021-06-03 12:45:03 -0300

    Temporarily slowing things down

--- raspi/src/Frame.h
+++ raspi/src/Frame.h
@@ -14,6 +14,8 @@ typedef struct Frame {
   }
 
   bool hasPixelChanged(uint32_t pixelId, Frame previousFrame) {
+    return true;  // TODO: FIX
+
     return !previousFrame.hasData() ||
            areDifferent(getColorOf(pixelId), previousFrame.getColorOf(pixelId));
   }
