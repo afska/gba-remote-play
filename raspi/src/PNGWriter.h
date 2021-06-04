65274e6df50b0d9479d505cc87b54afc3513bdee
Rodrigo Alfonso <rodri042@gmail.com>
2021-06-04 00:50:47 -0300

    Extracting constant

--- raspi/src/PNGWriter.h
+++ raspi/src/PNGWriter.h
@@ -5,6 +5,9 @@
 #include <stdio.h>
 #include "../lib/code/lodepng.h"
 
+#define PNG_SOURCE_COLORS 32
+#define PNG_TARGET_COLORS 256
+
 void WritePNG(std::string fileName,
               uint8_t* raw8BitPixels,
               uint16_t* raw15bppPalette,
@@ -17,11 +20,11 @@ void WritePNG(std::string fileName,
   state.info_png.color.colortype = LCT_PALETTE;
   state.info_png.color.bitdepth = 8;
 
-  for (int i = 0; i < 256; i++) {
+  for (int i = 0; i < PNG_TARGET_COLORS; i++) {
     uint16_t color = raw15bppPalette[i];
-    uint8_t r = ((color >> 0) & 0xff) * 256 / 32;
-    uint8_t g = ((color >> 5) & 0xff) * 256 / 32;
-    uint8_t b = ((color >> 10) & 0xff) * 256 / 32;
+    uint8_t r = ((color >> 0) & 0xff) * PNG_TARGET_COLORS / PNG_SOURCE_COLORS;
+    uint8_t g = ((color >> 5) & 0xff) * PNG_TARGET_COLORS / PNG_SOURCE_COLORS;
+    uint8_t b = ((color >> 10) & 0xff) * PNG_TARGET_COLORS / PNG_SOURCE_COLORS;
     uint8_t a = 0xff;
     lodepng_palette_add(&state.info_png.color, r, g, b, a);
     lodepng_palette_add(&state.info_raw, r, g, b, a);
