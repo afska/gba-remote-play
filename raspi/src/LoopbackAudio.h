cd26e86c9af6e7d3dd03c540084112229c961b55
Rodrigo Alfonso <rodri042@gmail.com>
2021-06-24 06:40:15 -0300

    Skipping audio chunks faster, using splice to avoid dummy reads

--- raspi/src/LoopbackAudio.h
+++ raspi/src/LoopbackAudio.h
@@ -1,18 +1,21 @@
 #ifndef LOOPBACK_AUDIO_H
 #define LOOPBACK_AUDIO_H
 
-#include <poll.h>
+#include <fcntl.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 #include <iostream>
-#include <stdexcept>
 #include <string>
 #include "Protocol.h"
 
 #define AUDIO_COMMAND                                                  \
   "ffmpeg -f alsa -i hw:0,1,0 -y -ac 1 -af 'aresample=18157' -strict " \
   "unofficial -c:a gsm -f gsm -loglevel quiet -"
+
+#define AUDIO_NULL "/dev/null"
 
 class LoopbackAudio {
  public:
@@ -21,30 +24,39 @@ class LoopbackAudio {
   uint8_t* loadChunk() {
     uint8_t* chunk = (uint8_t*)malloc(AUDIO_PADDED_SIZE);
 
-#define READ() fread(chunk, AUDIO_CHUNK_SIZE, 1, pipe)
-    while (isDataAvailable())
-      READ();
-    READ();
+    consumeExtraChunks();
+    fread(chunk, AUDIO_CHUNK_SIZE, 1, pipe);
 
     return chunk;
   }
 
-  ~LoopbackAudio() { pclose(pipe); }
+  ~LoopbackAudio() {
+    pclose(pipe);
+    close(nullFd);
+  }
 
  private:
   FILE* pipe;
+  int nullFd;
 
   void launchEncoder() {
     pipe = popen(AUDIO_COMMAND, "r");
+
     if (!pipe) {
       std::cout << "Error: cannot launch ffmpeg\n";
       exit(31);
     }
+
+    nullFd = open(AUDIO_NULL, O_RDWR);
   }
 
-  bool isDataAvailable() {
-    auto event = (struct pollfd){.fd = fileno(pipe), .events = POLLIN};
-    return poll(&event, 1, 0) == 1;
+  void consumeExtraChunks() {
+    uint32_t availableBytes = 0;
+    int fd = fileno(pipe);
+
+    ioctl(fd, FIONREAD, &availableBytes);
+    if (availableBytes > AUDIO_CHUNK_SIZE)
+      splice(fd, NULL, nullFd, NULL, availableBytes - AUDIO_CHUNK_SIZE, 0);
   }
 };
 
